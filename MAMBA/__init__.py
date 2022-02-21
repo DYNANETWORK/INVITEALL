48d73218ec26d051bdf7c58b2ffc69464c1e900b
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-22 00:40:57 +0530

    Update __init__.py

--- MAMBA/__init__.py
+++ MAMBA/__init__.py
@@ -27,9 +27,6 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 APP_ID = int(os.environ.get("APP_ID", 6))
  # 6 is a placeholder
 API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-ALIVE_NAME = set(
-        int(x) for x in os.environ.get("ALIVE_NAME", "MAMBA MAMBAX").split()
-    )
 ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
 STRING = os.environ.get("STRING", None)
 STRING2 = os.environ.get("STRING2", None)
